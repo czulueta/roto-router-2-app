3790f082d69a55e532982f2bdb2fdaaf81bd1868
czulueta <https://github.com/czulueta/weed.git>
2020-11-04 00:13:36 -0700

    added two more routes for ui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react"
 import {Link, Switch, Route} from "react-router-dom"
+import Home from "./Home"
+import About from "./About"
+import Gallery from "./Gallery"
+import Services from "./Services"
+import Contact from "./Contact"
 
 function App() {
   return (
     <div>
-      <Link to="/">Home</Link>
+      <Link exact to="/">Home</Link>
       <Link to="/about">About</Link>
+      <Link to="/services">Services</Link>
+      <Link to="/contact">Contact</Link>
       <Link to="/gallery">Gallery</Link>
       <Switch>
-          <Route path="/"><Home /></Route>
+          <Route exact path="/"><Home /></Route>
           <Route path="/about"><About /></Route>
           <Route path="/gallery"><Gallery /></Route>
+          <Route path="/services"><Services /></Route>
+          <Route path="/contact"><Contact /></Route>
       </Switch>
     </div>
   );
--- src/Contact.js
+++ src/Contact.js
@@ -1,10 +1,10 @@
 import React from "react"
 
-function Home() {
+function Contact() {
     return(
         <div>
-           <h1>Home</h1> 
+            <h1>Contact</h1>
         </div>
     )
 }
-export default Home
+export default Contact
