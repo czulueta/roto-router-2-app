2f89ba6bb721b57f8cdf1991ebfefdb4716e5f92
czulueta <https://github.com/czulueta/weed.git>
2020-11-03 23:59:58 -0700

    basic routes setup for home, about, and gallery

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import {Link, Switch, Route} from "react-router-dom"
 
 function App() {
   return (
     <div>
-      the basics
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/gallery">Gallery</Link>
+      <Switch>
+          <Route path="/"><Home /></Route>
+          <Route path="/about"><About /></Route>
+          <Route path="/gallery"><Gallery /></Route>
+      </Switch>
     </div>
   );
 }
